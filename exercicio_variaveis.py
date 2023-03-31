b6afcbd1cbc2f7acb602ea9a5d53b4521bb9a4f8
Victor-FM <victorfuscomartins@gmail.com>
2023-03-31 13:24:57 -0300

    teste

--- exercicio_variaveis.py
+++ exercicio_variaveis.py
@@ -2,12 +2,12 @@ nome = 'Victor'
 sobrenome = 'Fusco'
 idade = 18
 ano_nascimento = 2004
-maior_de_idade = idade >= 18
+maioridade = idade >= 18
 altura_metros = 1.74
 
 print('Nome:', nome)
 print('Sobrenome:', sobrenome)
 print('Idade:', idade)
 print('Ano de nascimento:', ano_nascimento)
-print('É maior de idade?:', maior_de_idade)
+print('É maior de idade?:', maioridade)
 print('Altura em metros:', altura_metros)
